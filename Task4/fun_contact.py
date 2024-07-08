8ac67364833178168185f18e6a927b3c3ba6779f
R.Anatolii <tv.weplay@gmail.com>
2024-07-07 18:21:56 -0700

    Tasck

--- Task4/fun_contact.py
+++ Task4/fun_contact.py
@@ -2,8 +2,9 @@
 Модуль оброботки контактов
 '''
 import re
+from colorama import Fore, Style
 
-def is_phone_number(phone):
+def is_phone_number(phone) -> tuple[bool, str]:
     phone_number = re.sub(r"[^0-9]", "", phone)
     if not re.search(r'\d{10}$', phone_number):
         return False, phone
@@ -18,12 +19,12 @@ def add_contact(args: list, contacts: dict) -> str:
         name, phone = args
         is_phon, num_phon = is_phone_number(phone)
         if not is_phon:
-            return f"Phone number {num_phon} is incorrect. Contact not added"
+            return Fore.RED + f"Phone number '{num_phon}' is incorrect. Contact not added" + Fore.RESET
         if contacts.get(name, False):
-            return f"The name '{name}' is in the dictionary"
+            return Fore.RED + f"The name '{name}' is in the dictionary" + Fore.RESET
         
         contacts[name] = num_phon
-        return f"Contact added '{name}' phon {contacts[name]}."
+        return Fore.GREEN + f"Contact added '{name}' phon {contacts[name]}." +  Fore.RESET
 
     except Exception as e:
         return f"Error: {e}"
@@ -34,14 +35,14 @@ def change_contact(args: list, contacts: dict) -> str:
         if len(args) != 2: return f"Requires 2 arguments 'name' and 'phone'"
         name, phone = args
         if name not in contacts:
-            return "Contact does not exist"
+            return Fore.RED + "Contact does not exist" + Fore.RESET
         
         is_phon, num_phon = is_phone_number(phone)
         if not is_phon:
-            return f"Phone number {num_phon} is incorrect. Contact not added"
+            return Fore.RED + f"Phone number {num_phon} is incorrect. Contact not update" + Fore.RESET
 
         contacts[name] = num_phon
-        return f"Contact update '{name}' phon {contacts[name]}."
+        return Fore.GREEN + f"Contact update '{name}' phon {contacts[name]}." + Fore.RESET
 
     except Exception as e:
         return f"Error: {e}"
@@ -51,12 +52,12 @@ def get_contact(args: list, contacts: dict) -> str:
     try:
         if len(args) != 1: return f"Requires 1 arguments 'name'"
         name = args[0]
-        if name not in contacts: return f"Contact '{name}' does not exist"
-        return f"'{name}' phon number {contacts[name]}"
+        if name not in contacts: return Fore.RED + f"Contact '{name}' does not exist" + Fore.RESET
+        return Fore.YELLOW + f"'{name}' phon number {contacts[name]}" + Fore.RESET
     except Exception as e:
         return f"Error: {e}"
 
 
 def all_contact(contacts: dict) -> str:
-    if len(contacts) == 0: return f"Container is empty"
-    return [f"{name}: {phone}" for name, phone in contacts.items()]
+    if len(contacts) == 0: return  Fore.RED + f"Container is empty" +  Fore.RESET
+    return [f"{name}: {phone}" for name, phone in contacts.items()] 
